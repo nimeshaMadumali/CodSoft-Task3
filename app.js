ef43e2d6100a882b6e5436bd0c2a79ac17ee9940
nimeshaMadumali <madumalinimesha070@gmail.com>
2024-08-13 23:09:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const output = document.querySelector(".output");
+const result = document.querySelector(".result");
+const keys = document.querySelectorAll("button");
+
+keys.forEach(key=>{
+    key.addEventListener("click", calculator);
+});
+
+function calculator(){
+    let buttonText = this.innerText;
+    if(buttonText === "AC"){
+        output.innerText ="";
+        result.innerText ="0";
+        result.style.animation = "";
+        output.style.animation = "";
+        return;
+    }
+
+    if(buttonText === "DEL"){
+        output.textContent = output.textContent.substr(0, output.textContent.length-1);
+        return;
+    }
+
+    if(buttonText === "="){
+        result.innerText = eval(output.innerText);
+        result.style.animation = "big 1s ease-in-out";
+        output.style.animation = "small 0.5s ease-in-out";
+        result.style.animationFillMode = "forwards";
+        output.style.animationFillMode = "forwards";
+    }
+
+    else{
+        output.textContent += buttonText;
+        return;
+    }
+}
